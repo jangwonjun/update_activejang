d6c14ba9cbf01258699331f800861c3ae5407082
wonjunJang <nuriyooyoung@gmail.com>
2023-06-02 05:51:52 +0900

    brand 링크 수정

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ app  = Flask(__name__, static_url_path='/static')
 def main():
     return render_template('index.html')
 
+@app.route('/consulting_jang')
+def consultingjang():
+    return render_template('consultingjang.html')
+
+@app.route('/edu_jang')
+def edujang():
+    return render_template('edujang.html')
+
+@app.route('/volunteer_jang')
+def volunteerjang():
+    return render_template('volunteerjang.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=env.DEBUG, port=env.PORT)
