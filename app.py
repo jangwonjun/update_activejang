78d2c56f8f90a6690c8f333d51a813638fc8cbac
wonjunJang <nuriyooyoung@gmail.com>
2023-06-02 18:09:56 +0900

    static 수정

--- app.py
+++ app.py
@@ -4,23 +4,34 @@ from proctitle import setproctitle
 
 setproctitle.setproctitle(env.PROC_NAME)
 
-app  = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    nav_item = (
+        "Introduce", "portfolio", "Projecting", "Making Film", "Brand"
+    )
+    return render_template('index.html', nav_item=nav_item)
+
 
 @app.route('/consulting_jang')
 def consultingjang():
     return render_template('consultingjang.html')
 
+
 @app.route('/edu_jang')
 def edujang():
-    return render_template('edujang.html')
+    nav_item = (
+        "Introduce", "Review", "portfolio", "Brand"
+    )
+    return render_template('edujang.html', nav_item=nav_item)
+
 
 @app.route('/volunteer_jang')
 def volunteerjang():
     return render_template('volunteerjang.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=env.DEBUG, port=env.PORT)
